6effb146fbadc5dd5bb16947b19e3be50add6999
Said van de Klundert <said.van.de.klundert@ibm.com>
2021-07-03 14:47:12 +0200

    python

--- fapi/api/strings_and_stuff.py
+++ fapi/api/strings_and_stuff.py
@@ -7,3 +7,9 @@ router = fastapi.APIRouter()
 @router.get("/api/words/upper/{word}")
 def upper(word: str):
     return f"your word in uppercase: {word.upper()}"
+
+
+# http://127.0.0.1/api/words/word/{word}
+@router.get("/api/words/word/{word}")
+def word(word: str, second_word: str = "default"):
+    return f"{word} 2nd word {second_word}"
