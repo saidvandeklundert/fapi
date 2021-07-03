fc99ebbf784b30df373208022c6543e46de0f5d6
Said van de Klundert <said.van.de.klundert@ibm.com>
2021-07-03 08:45:10 +0200

    adding stuff

--- fapi/main.py
+++ fapi/main.py
@@ -16,9 +16,9 @@ def give_number():
 # http://127.0.0.1/api/calculate?x=2&y=8&z=10
 @api.get("/api/calculate")
 def sum(x: int, y: int, z: Optional[int] = None):
-    if not str(z).isdigit():
+    if z and z > 100:
         return fastapi.Response(
-            content='{ "error" : "ERROR: z cannot be a digit" ',
+            content='{ "error" : "ERROR: z cannot be bigger then 100" ',
             media_type="application/json",
             status_code=400,
         )
