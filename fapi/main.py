890fdcae49b7c83470dbda8fd738f39f112c3e56
Said van de Klundert <said.van.de.klundert@ibm.com>
2021-07-03 08:48:41 +0200

    adding stuff

--- fapi/main.py
+++ fapi/main.py
@@ -16,12 +16,17 @@ def give_number():
 # http://127.0.0.1/api/calculate?x=2&y=8&z=10
 @api.get("/api/calculate")
 def sum(x: int, y: int, z: Optional[int] = None):
-    if z and z > 100:
+    if z and z == 20:
         return fastapi.Response(
-            content='{ "error" : "ERROR: z cannot be bigger then 100" ',
+            content='{ "error" : "ERROR: z cannot be bigger then 100"}',
             media_type="application/json",
             status_code=400,
         )
+    elif z and z == 30:
+        return fastapi.responses.JSONResponse(
+            content={"error": "ERROR: z cannot be bigger then 100"}, status_code=400
+        )
+
     ret = {"x": x, "y": y, "z": z}
     value = x + y
     if z:
