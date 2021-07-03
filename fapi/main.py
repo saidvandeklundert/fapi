2fa4a65d1185d6d0bba5be63ac18cc67f39ca7eb
Said van de Klundert <said.van.de.klundert@ibm.com>
2021-07-03 08:30:11 +0200

    adding stuff

--- fapi/main.py
+++ fapi/main.py
@@ -16,12 +16,12 @@ def give_number():
 # http://127.0.0.1/api/calculate?x=2&y=8&z=10
 @api.get("/api/calculate")
 def sum(x: int, y: int, z: Optional[int] = None):
-
-    ret = {"x": x, "y": y, "z": z, "value": None}
+    ret = {"x": x, "y": y, "z": z}
     value = ret["value"]
     value = x + y
     if z:
         value += z
+    ret["value"] = value
     return ret
 
 
