2637fe86e58400b6bd910c9474047749e01c6259
Said van de Klundert <said.van.de.klundert@ibm.com>
2021-07-03 14:31:04 +0200

    python

--- fapi/views/home.py
+++ fapi/views/home.py
@@ -5,11 +5,11 @@ from starlette.templating import Jinja2Templates
 router = fastapi.APIRouter()
 templates = Jinja2Templates("templates")
 # http://127.0.0.1/
-@api.get("/")
+@router.get("/")
 def index(request: Request):
     return templates.TemplateResponse("index.html", {"request": request})
 
 
-@api.get("/favicon.ico")
+@router.get("/favicon.ico")
 def favicon():
     return fastapi.responses.RedirectResponse(url="/static/img/ma_face.jpeg")
