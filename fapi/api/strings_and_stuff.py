926a86adf984833ed8f32fd47cfd16be8aa915cf
Said van de Klundert <said.van.de.klundert@ibm.com>
2021-07-04 09:10:50 +0200

    python

--- fapi/api/strings_and_stuff.py
+++ fapi/api/strings_and_stuff.py
@@ -36,9 +36,7 @@ async def asyncword(word: str, second_word: str = "default"):
 
 # http://127.0.0.1/api/words/backgrounword/{word}?second_word=secondword
 @router.post("/api/words/backgrounword/{word}")
-async def backgrounword(
-    word: str, second_word: str = "default", background_tasks: BackgroundTasks
-):
+async def backgrounword(word: str, second_word: str, background_tasks: BackgroundTasks):
 
     background_tasks.add_task(background_word, word)
     return f"{word} 2nd word {second_word}"
