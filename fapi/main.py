7ce7ca0ec6323efdcb1e1498bb5691287e72cddf
Said van de Klundert <said.van.de.klundert@ibm.com>
2021-07-05 05:56:41 +0200

    python

--- fapi/main.py
+++ fapi/main.py
@@ -5,6 +5,7 @@ from starlette.staticfiles import StaticFiles
 from views import home
 from api import math_and_numbers
 from api import strings_and_stuff
+from fastapi import BackgroundTasks
 
 api = fastapi.FastAPI()
 
@@ -22,6 +23,18 @@ def configure_routing():
 
 # swagger
 # http://127.0.0.1/docs
+
+
+def write_notification(email: str, message=""):
+    with open("log.txt", mode="w") as email_file:
+        content = f"notification for {email}: {message}"
+        email_file.write(content)
+
+
+@api.post("/send-notification/{email}")
+async def send_notification(email: str, background_tasks: BackgroundTasks):
+    background_tasks.add_task(write_notification, email, message="some notification")
+    return {"message": "Notification sent in the background"}
 
 
 if __name__ == "__main__":
