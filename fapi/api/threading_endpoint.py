fc6ce25bd2502a618ef1f5e13cc0d438ffd54119
Said van de Klundert <said.van.de.klundert@ibm.com>
2021-07-05 20:24:39 +0200

    python

--- fapi/api/threading_endpoint.py
+++ fapi/api/threading_endpoint.py
@@ -14,44 +14,38 @@ curl -X 'POST' \
 """
 
 
-@router.post("/api/threading/background/{words}")
-async def backgroundword(words: str, background_tasks: BackgroundTasks):
+@router.post("/api/threading/background/{word}")
+async def backgroundword(word: str, background_tasks: BackgroundTasks):
     print("starting background task:")
-    background_tasks.add_task(back_ground_func, words)
+    background_tasks.add_task(back_ground_func, word)
     return f"words are being written in the background"
 
 
-def back_ground_func(words: str):
-    futures = threading()
-    s = ""
+def back_ground_func(word: str):
+    futures = threading(word)
+
     for item in futures:
-        s += item.result()
-    with open("/var/log/log.txt", mode="a") as words_file:
-        content = f"{words} {s}\n"
-        words_file.write(content)
+        print(item)
 
 
-def io_bound_function(parameter):
+def io_bound_function(host: str, word: str):
     """
     Simulates an IO-bound function using time.sleep(3).
     """
-    print(f"io_bound_function called with argument {parameter}")
+    print(f"io_bound_function called with argument {host}")
     time.sleep(3)
-    print(f"io_bound_function with argument {parameter} ended")
-    return parameter
+    with open("/var/log/log.txt", mode="a") as words_file:
+        content = f"{host}: command {word}\n"
+        words_file.write(content)
+    print(f"io_bound_function with argument {host} ended")
+    return True
 
 
-def threading():
-    """
-    Example where a function that requires an arg
-     is passed to a ThreadPoolExecutor.
-    In this case, the 'max_workers' is configurable.
-    """
-
+def threading(word: str):
     futures = []
     with ThreadPoolExecutor(max_workers=3) as executor:
-        argument_list = ["this ", "will ", "appear ", "in ", "the ", "log"]
-        for argument in argument_list:
-            futures.append(executor.submit(io_bound_function, argument))
+        host_list = ["r1 ", "r2 ", "r3 ", "r4", "r5 ", "r6"]
+        for host in host_list:
+            futures.append(executor.submit(io_bound_function, host, word))
 
     return futures
