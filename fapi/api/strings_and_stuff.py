d6936880de0465d24df279bc7bef405eb2634132
Said van de Klundert <said.van.de.klundert@ibm.com>
2021-07-05 19:58:05 +0200

    python

--- fapi/api/strings_and_stuff.py
+++ fapi/api/strings_and_stuff.py
@@ -1,7 +1,18 @@
 import fastapi
+from fastapi import BackgroundTasks
 import time
+from concurrent.futures import ThreadPoolExecutor
 import asyncio
-from models.errors import ValidationError
+
+from pydantic.error_wrappers import ValidationError
+
+
+class ValidationError(Exception):
+    def __init__(self, error_msg: str, status_code: int):
+        super().__init__(error_msg)
+        self.status_code = status_code
+        self.error_msg = error_msg
+
 
 router = fastapi.APIRouter()
 
@@ -26,6 +37,50 @@ def word(word: str, second_word: str = "default"):
 async def asyncword(word: str, second_word: str = "default"):
     await asyncio.sleep(2)
     return f"{word} 2nd word {second_word}"
+
+
+# http://127.0.0.1/api/words/backgroundword/backgroundtask
+@router.post("/api/words/backgroundword/{words}")
+async def backgroundword(words: str, background_tasks: BackgroundTasks):
+
+    background_tasks.add_task(background_word, words)
+    return f"words are being written in the background"
+
+
+def background_word(words: str):
+    futures = example_threadpool()
+    s = ""
+    for item in futures:
+        s += item.result()
+    with open("/var/log/log.txt", mode="w") as words_file:
+        content = f"{words} {s}"
+        words_file.write(content)
+
+
+def io_bound_function(parameter):
+    """
+    Simulates an IO-bound function using time.sleep(3).
+    """
+    print(f"io_bound_function called with argument {parameter}")
+    time.sleep(3)
+    print(f"io_bound_function with argument {parameter} ended")
+    return parameter
+
+
+def example_threadpool():
+    """
+    Example where a function that requires an arg
+     is passed to a ThreadPoolExecutor.
+    In this case, the 'max_workers' is configurable.
+    """
+
+    futures = []
+    with ThreadPoolExecutor(max_workers=3) as executor:
+        argument_list = ["this ", "will ", "appear ", "in ", "the ", "log"]
+        for argument in argument_list:
+            futures.append(executor.submit(io_bound_function, argument))
+
+    return futures
 
 
 # http://127.0.0.1/api/words/correctword/{word}
